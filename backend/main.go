e82f9b89552ce8ee2c4487579c99edd624059e50
Fabricio Asat <fabricioasat00@gmail.com>
2023-04-12 19:30:07 -0300

    implemented cors

--- backend/main.go
+++ backend/main.go
@@ -6,12 +6,20 @@ import (
 	"github.com/FabricioAsat/chat-app-go-react/routers"
 	"github.com/FabricioAsat/chat-app-go-react/services"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	services.DotEnvInit()
 	PORT := os.Getenv("PORT")
 	app := fiber.New()
+
+	// Cors
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowMethods: "GET, POST, PUT, DELETE",
+	}))
 
 	// Routes
 	routers.UserRouter(app)
