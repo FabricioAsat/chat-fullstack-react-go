7b8405fc8a17661e9d6d9f74c0244cfe4e1bc9ea
Fabricio Asat <fabricioasat00@gmail.com>
2023-04-12 07:56:02 -0300

    Register page design created

--- frontend/src/components/LayoutLogReg.jsx
+++ frontend/src/components/LayoutLogReg.jsx
@@ -1,60 +1,73 @@
-import lock from "../assets/svg/lock.svg";
-import easy from "../assets/svg/easy.svg";
-import secure from "../assets/svg/secure.svg";
+import go from "../assets/svg/go.svg";
+import tailwind from "../assets/svg/tailwind.svg";
+import react from "../assets/svg/react.svg";
+import vite from "../assets/svg/vite.svg";
+
 import logo from "../assets/svg/logo.svg";
 
 export const LayoutLogReg = ({ children }) => {
 	return (
 		<div className={`w-full max-w-7xl mx-auto h-full grid grid-cols-2`}>
-			<article className="flex flex-col justify-start gap-y-16 bg-blue-800 p-5">
-				<span className="flex justify-center items-end text-4xl font-bold">
+			<article className="flex flex-col justify-start gap-y-10 bg-blue-700 p-5">
+				<span className="flex justify-center items-end text-4xl font-bold my-10">
 					Ch
 					<img src={logo} alt="Easy" className="h-7 pb-px" />
 					tting
 				</span>
 
-				<section className="flex gap-x-5 max-w-lg mx-auto w-full">
-					<picture className="h-20 w-24 rounded-lg flex items-center justify-center bg-white/10">
-						<img src={easy} alt="Easy" className="h-10" />
+				<section className="flex gap-x-7 max-w-lg mx-auto w-full">
+					<picture className="h-20 w-24 rounded-lg flex items-center justify-center bg-black/20">
+						<img src={vite} alt="Vitejs" className="h-10" />
 					</picture>
 
 					<aside className="w-full">
-						<h2 className="text-base font-bold text-white">Fácil de usar</h2>
+						<h2 className="text-base font-bold text-white mb-px">Vite.js</h2>
 						<p className="text-sm text-neutral-300">
-							La web posee un diseño amigable con el usuario. Lo que le permite utilizarla
-							facilmente sin problemas.
+							Empaquetado rápido y moderno, liviano y sencillo.
 						</p>
 					</aside>
 				</section>
 
-				<section className="flex gap-x-5 max-w-lg mx-auto w-full">
-					<picture className="h-20 w-24 rounded-lg flex items-center justify-center bg-white/10">
-						<img src={lock} alt="Easy" className="h-10" />
+				<section className="flex gap-x-7 max-w-lg mx-auto w-full">
+					<picture className="h-20 w-24 rounded-lg flex items-center justify-center bg-black/20">
+						<img src={tailwind} alt="Easy" className="h-10" />
 					</picture>
 
 					<aside className="w-full">
-						<h2 className="text-base font-bold text-white">Cifrado extremo</h2>
+						<h2 className="text-base font-bold text-white mb-px">Tailwind CSS</h2>
 						<p className="text-sm text-neutral-300">
-							Tus mensajes y contraseñas se cifran de extremo a extremo haciendo imposible ver tus
-							datos.
+							Framework CSS básico y sencillo capaz de generar un diseño básico y bonito.
 						</p>
 					</aside>
 				</section>
 
-				<section className="flex gap-x-5 max-w-lg mx-auto w-full">
-					<picture className="h-20 w-24 rounded-lg flex items-center justify-center bg-white/10">
-						<img src={secure} alt="Easy" className="h-10" />
+				<section className="flex gap-x-7 max-w-lg mx-auto w-full">
+					<picture className="h-20 w-24 rounded-lg flex items-center justify-center bg-black/20">
+						<img src={react} alt="Easy" className="h-10" />
 					</picture>
 
 					<aside className="w-full">
-						<h2 className="text-base font-bold text-white">Alta Seguridad</h2>
+						<h2 className="text-base font-bold text-white mb-px">React.js</h2>
 						<p className="text-sm text-neutral-300">
-							Protección de última generación ante hackeos externos.
+							Librería de maquetado fácil de utilizar para proyectos como este.
+						</p>
+					</aside>
+				</section>
+
+				<section className="flex gap-x-7 max-w-lg mx-auto w-full">
+					<picture className="h-20 w-24 rounded-lg flex items-center justify-center bg-black/20">
+						<img src={go} alt="Easy" className="h-10" />
+					</picture>
+
+					<aside className="w-full">
+						<h2 className="text-base font-bold text-white mb-px">Go</h2>
+						<p className="text-sm text-neutral-300">
+							Lenguaje usado en el backend, rápido y seguro.
 						</p>
 					</aside>
 				</section>
 			</article>
-			<article className="bg-neutral-950">{children}</article>
+			<article className="bg-neutral-950 py-10 px-5">{children}</article>
 		</div>
 	);
 };
